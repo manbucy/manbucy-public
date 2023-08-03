4439c2e646ddfe531c0d770d05d92068dcda5668
manbucy <1412039603@qq.com>
2023-08-03 09:57:29 +0800

    init

---
+++ src/main/java/io/github/manbucy/UtilException.java
@@ -0,0 +1,29 @@
+package io.github.manbucy;
+
+public class UtilException extends RuntimeException {
+    private static final long serialVersionUID = 8247610319171014183L;
+
+    public UtilException(Throwable e) {
+        super(e.getMessage(), e);
+    }
+
+    public UtilException(String message) {
+        super(message);
+    }
+
+    public UtilException(String messageTemplate, Object... params) {
+        super(String.format(messageTemplate, params));
+    }
+
+    public UtilException(String message, Throwable throwable) {
+        super(message, throwable);
+    }
+
+    public UtilException(String message, Throwable throwable, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, throwable, enableSuppression, writableStackTrace);
+    }
+
+    public UtilException(Throwable throwable, String messageTemplate, Object... params) {
+        super(String.format(messageTemplate, params), throwable);
+    }
+}
